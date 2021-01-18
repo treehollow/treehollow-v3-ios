c45c2a68c18ed92943de0dee1ed1e044bc3f60fa
梁业升 <liang2kl@outlook.com>
2021-01-18 23:01:15 +0800

    Add ConfigRequest.swift

---
+++ Hollow/Hollow/Model/Net/Config/ConfigRequest.swift
@@ -0,0 +1,30 @@
+//
+//  ConfigRequest.swift
+//  Hollow
+//
+//  Created by liang2kl on 2021/1/18.
+//  Copyright © 2021 treehollow. All rights reserved.
+//
+
+import Foundation
+
+// No configurations for config request as it takes no parameters.
+
+/// Result of requesting system config.
+struct ConfigRequestResult {
+    var name: String
+    // FIXME: Int or String
+    var reCAPTCHAV3Key: Int
+    var reCAPTCHAV2Key: Int
+    var apiRoot: String
+    var tosUrl: String
+    var privacyUrl: String
+    var contactEmail: String
+    var emailSuffixes: [String]
+    var announcement: String
+    var foldTags: [String]
+    var sendableTags: [String]
+    var imageBaseUrl: String
+    var imageBaseUrlBak: String
+    // FIXME: existing ios frontend version
+}
