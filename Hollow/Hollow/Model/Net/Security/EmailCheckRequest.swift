b27ccdb943e773a4ec733d1726f02046f8c288a1
PKU Hollow <contact@pkuhollow.com>
2021-01-29 22:48:19 +0800

    add EmailCheckRequest

--- Hollow/Hollow/Model/Net/Security/EmailCheckRequest.swift
+++ Hollow/Hollow/Model/Net/Security/EmailCheckRequest.swift
@@ -11,28 +11,30 @@ import Networking
 /// Configurations for email check
 struct EmailCheckRequestConfiguration {
     /// `reCAPTCHA` version
-    enum ReCAPTCHAVersion: String {
-        case v2 = "v2"
-        case v3 = "v3"
-    }
+//    NO USE!
+//    enum ReCAPTCHAVersion: String {
+//        case v2 = "v2"
+//        case v3 = "v3"
+//    }
     /// User's email to be checked, required
     var email: String
     /// User's old thuhole token, only needed for old user signup
     /// using web frontend, optional
-    var oldToken: String?
+    // var oldToken: String?
     /// Info of `reCAPTCHA`, optional
-    var reCAPTCHAInfo: (token: String, version: ReCAPTCHAVersion)?
+    // NO USE
+    //var reCAPTCHAInfo: (token: String, version: ReCAPTCHAVersion)?
     /// constant used for http request
-    var requestConst: GetConfigRequestResult
+    var hollowConfig: GetConfigRequestResult
 }
 
-/// Result of email checking.
-struct EmailCheckRequestResult {
+/// Result data of email checking.
+struct EmailCheckRequestResult: Codable{
     /// Result type of email checking.
     ///
     /// Please init with `EmailCheckRequest.ResultType(rawValue: Int)`, and should
     /// show error with `nil`, which means receiving negative code.
-    enum ResultType: Int {
+    enum ResultType: Int,Codable {
         /// Old user, shuold input password to login.
         case oldUser = 0
         /// New user, should check email for valid code and create password to sign up.
@@ -44,21 +46,65 @@ struct EmailCheckRequestResult {
     }
     
     /// The type of result received.
-    var type: ResultType
+    var code: ResultType
     /// Error message, if error occured.
-    var message: String?
+    var msg: String?
 }
-//
-//class EmailCheckRequest: Request{
-//    typealias Configuration = EmailCheckRequestConfiguration
-//    typealias Result = EmailCheckRequestResult
-//    typealias ResultData = Result
-//    typealias Error = EmailCheckRequestError
-//    enum EmailCheckRequestError: RequestError{
-//    }
-//    var configuration: EmailCheckRequestConfiguration
-//    required init(configuration: EmailCheckRequestConfiguration) {
-//        self.configuration = configuration
-//    }
-//
-//}
+
+class EmailCheckRequest: Request{
+   
+    typealias Configuration = EmailCheckRequestConfiguration
+    typealias Result = EmailCheckRequestResult
+    typealias ResultData = Result
+    typealias Error = EmailCheckRequestError
+    enum EmailCheckRequestError: RequestError{
+        case decodeError
+        case other(description: String)
+        var description: String{
+            switch self {
+            case .decodeError: return "Decode failed"
+            case .other(let description): return description
+            }
+        }
+    }
+    var configuration: EmailCheckRequestConfiguration
+    required init(configuration: EmailCheckRequestConfiguration) {
+        self.configuration = configuration
+    }
+    
+    func performRequest(completion: @escaping (Result?, EmailCheckRequestError?) -> Void) {
+        let parameters = ["email" : self.configuration.email]
+        let urlPath = "/v3/security/login/check_email" + self.configuration.hollowConfig.urlSuffix!
+        // URL must be leagle !
+        let networking = Networking(baseURL: self.configuration.hollowConfig.apiRoot)
+        networking.post(urlPath, parameters: parameters) { result in
+            let jsonDecoder = JSONDecoder()
+            jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+           // Successfull post using `application/json` as `Content-Type`
+            switch result{
+            case .success(let response):
+                do {
+                    // FIXME: NOT TESTED YET
+                    let result = try jsonDecoder.decode(EmailCheckRequestResult.self, from: response.data)
+                    completion(result,nil)
+                } catch {
+                    completion(nil,.decodeError)
+                    return
+                }
+                // If we need headers or response status code we can use the HTTPURLResponse for this.
+                //let headers = response.headers // [String: Any]
+            case .failure(let response):
+                // Non-optional error ✨
+                let errorCode = response.error.code
+
+                // Our backend developer told us that they will send a json with some
+                // additional information on why the request failed, this will be a dictionary.
+                let errorResponse = String(data: response.data, encoding: .utf8) ?? ""// BOOM, no optionals here [String: Any]
+                let errormsg = String(errorCode) + errorResponse
+                completion(nil,.other(description: errormsg))
+                // We want to know the headers of the failed response.
+                //let headers = response.headers // [String: Any]
+            }
+        }
+    }
+}
