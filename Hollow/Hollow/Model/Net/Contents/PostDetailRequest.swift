86c0788d208abffbbfea82f2b03084f97069bd57
PKU Hollow <contact@pkuhollow.com>
2021-02-20 23:57:44 +0800

    implement PostDetailRequest

--- Hollow/Hollow/Model/Net/Contents/PostDetailRequest.swift
+++ Hollow/Hollow/Model/Net/Contents/PostDetailRequest.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 import Alamofire
-import Cache
 
 struct PostDetailRequestConfiguration {
     var apiRoot: [String]
@@ -16,8 +15,6 @@ struct PostDetailRequestConfiguration {
     var postId: Int
     /// if this param is given and there's no new comment, the return code would
     /// be 1 (`.successWithNoNewComment`), while comments and post would be none.
-    // dont need to pass this
-    // var oldUpdateTimestamp: Int?
 }
 
 struct PostDetailRequestResult: DefaultRequestResult {
@@ -27,15 +24,7 @@ struct PostDetailRequestResult: DefaultRequestResult {
     var data: [Comment]?
 }
 
-struct PostDetailRequestResultData {
-    enum ResultType: Int {
-        case success = 0
-        case successWithNoNewComment = 1
-    }
-    
-    var type: ResultType
-    var postWrap: PostDataWrapper
-}
+typealias PostDetailRequestResultData = PostDataWrapper
 
 struct PostDetailRequest: DefaultRequest {
     
@@ -56,27 +45,84 @@ struct PostDetailRequest: DefaultRequest {
             "TOKEN": self.configuration.token,
             "Accept": "application/json"
         ]
-        var parameters: [String : String] = [
-            "pid" : configuration.postId.string
+        var parameters: [String : Encodable] = [
+            "pid" : configuration.postId
         ]
         
-        let commentsTimestamp: Storage<Int, Int>
+        if let oldupdated = PostCache().getTimestamp(postId: configuration.postId) {
+            parameters["old_updated_at"] = oldupdated
+        }
         
-        if let oldupdated = self.configuration.oldUpdateTimestamp {
-            parameters["old_updated_at"] = oldupdated.string
+        let resultToResultData: (Result) -> ResultData? = { result in
+            
+            var postWrapper: PostDataWrapper? = nil
+            
+            if result.code == 1 { // use cached result
+                // if return cache hit, then post must be in cache
+                postWrapper = PostDataWrapper(post: PostCache().getPost(postId: configuration.postId)!, citedPost: nil)
+            } else {
+                // use new result
+                if let post = result.post, let comments = result.data {
+                    let postData = post.toPostData(comments: comments.map{ $0.toCommentData() })
+                    // postdetail dont need cited post
+                    postWrapper = PostDataWrapper(post: postData, citedPost: nil)
+                    // return postWrapper without image and cited post
+                    completion(postWrapper,nil)
+                    // cache comments
+                    PostCache().updateTimestamp(postId: configuration.postId, timestamp: post.updatedAt)
+                    // force unwrap postWrapper
+                    PostCache().updatePost(postId: configuration.postId, postdata: postWrapper!.post)
+                }
+            }
+            
+            // load post image
+            
+            if let url = postWrapper?.post.hollowImage?.imageURL {
+                ImageDownloader.downloadImage(urlBase: configuration.imageBaseURL, urlString: url, imageCompletionHandler: {image in
+                    if let image = image {
+                        postWrapper?.post.hollowImage?.image = image
+                        completion(postWrapper,nil)
+                        PostCache().updatePost(postId: configuration.postId, postdata: postWrapper!.post)
+                    } else {
+                        completion(postWrapper,.imageLoadingFail(postID: (postWrapper?.post.postId)!))
+                    }
+                })
+            }
+            
+            // load comments image
+            
+            if let comments = postWrapper?.post.comments {
+                for index in comments.indices {
+                    if let url = comments[index].image?.imageURL {
+                        ImageDownloader.downloadImage(
+                            urlBase: configuration.imageBaseURL,
+                            urlString: url,
+                            imageCompletionHandler: { image in
+                                if let image = image {
+                                    postWrapper?.post.comments[index].image?.image = image
+                                    completion(postWrapper, nil)
+                                    PostCache().updatePost(postId: configuration.postId, postdata: postWrapper!.post)
+                                } else {
+                                    // report image loading fail
+                                    completion(postWrapper,.commentImageLoadingFail(
+                                        postID: (postWrapper?.post.postId)!,
+                                        commentID: comments[index].commentId
+                                    )
+                                    )
+                                }
+                            }
+                        )
+                    }
+                }
+            }
+            return postWrapper
         }
         
         performRequest(urlBase: configuration.apiRoot,
                        urlPath: urlPath,
+                       headers: headers,
                        method: .get,
-                       resultToResultData: { result in
-                        if let post = result.post, let comments = result.data {
-                            // postdetail dont need cited post
-                            var postWrapper = PostDataWrapper(post: post.toPostData(comments: comments.map{ $0.toCommentData() }), citedPost: nil)
-                            completion(PostDetailRequestResultData(type: post.updatedAt == oldupdated ? .success : .successWithNoNewComment, postWrap: postWrapper),nil)
-                        }
-                       },
+                       resultToResultData: resultToResultData,
                        completion: completion)
     }
-    
 }
--- Hollow/Hollow/Model/Utilities/CommentCache.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CommentCache.swift
-//  Hollow
-//
-//  Created by aliceinhollow on 2/20/21.
-//  Copyright © 2021 treehollow. All rights reserved.
-//
-
-import Foundation
